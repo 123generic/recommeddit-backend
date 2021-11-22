c34d7872fc5ad5d0e2ccd9ee46babdf650ca266e
KarimBenyassine1 <karimbeny02@gmail.com>
2021-11-21 21:21:44 -0800

    added google auto suggest function

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
 #!/usr/bin/python
-
+import requests
+import sys
+import json
 import asyncio
-
 import recommendations
 
+
+def auto_suggest(request):
+    '''
+    if request.args and 'query' in request.args:
+        query = request.args.get('query')
+    else:
+        query = ""
+    '''
+    query = request
+
+    cursorPoint = len(query)+1
+    newQuery = "%20".join(query.split())
+
+    url = "https://www.google.com/complete/search?q={}%20reddit&cp={}&client=gws-wiz&xssi=t&hl=en&authuser=0&dpr=2".format(newQuery, cursorPoint)
+
+    res = requests.get(url).text[5:]
+    parsed = json.loads(res)[0]
+
+    newResult = [element[0].replace("reddit", "") for element in parsed]
+
+    return newResult, 200, {'Access-Control-Allow-Origin': '*'}
 
 def search(request):
     if request.args and 'query' in request.args:
@@ -18,12 +40,12 @@ def search(request):
 def main():
     # assume first argument is query. Default query is 'C++ IDE'
     try:
-        import sys
         query = sys.argv[1]
     except IndexError:
-        query = "similar movies to inception"
+        query = "Best C++ IDE"
 
-    results = asyncio.run(recommendations.get_recommendations(query))
+    #results = asyncio.run(recommendations.get_recommendations(query))
+    results = auto_suggest(query)
 
     print(results)
 
